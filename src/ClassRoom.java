318c791fd179ab016b7771ea9425a7f6be630b88
Santiago Alzate <salzatec1@localhost.localdomain>
2020-05-29 16:52:33 -0500

    ahora si esta bien

--- src/ClassRoom.java
+++ src/ClassRoom.java
@@ -1,3 +1,27 @@
-public class ClassRoom {
 
+import java.util.Map;
+import java.util.HashMap;
+
+public class ClassRoom{
+
+    final int id;
+
+    final String type;
+
+    final int block;
+
+    final int capacity;
+
+    HashMap<char, HashMap<String, Group>> bookings;
+
+    public ClassRoom(int id, String type, int block, boolean access){
+        this.id = id;
+        this.type = type;
+        this.block = block;
+        this.capacity = capacity;
+        this.bookings = new HashMap<char, new HashMap<String, Group>;
+
+    }
 }
+
+
