dd9e32a96e6232110b7d18a22abe61cbd4526fea
jpgomezt <jpgomeztriana@gmail.com>
2020-05-29 16:38:28 -0500

    Add getClassrooms method

--- src/Reader.java
+++ src/Reader.java
@@ -3,33 +3,49 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Reader {
-    public static HashMap<Integer, Student> getStudens(){
-        HashMap<Integer, Student> listStudents = new HashMap<>();
+
+    public static HashMap<Integer, ClassRoom> getClassrooms() {
+        HashMap<Integer, ClassRoom> listClassRooms = new HashMap<>();
+        try {
+            Scanner scanner = new Scanner(new File("CSV/aulas.csv"));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                Scanner lineScanner = new Scanner(line);
+                lineScanner.useDelimiter(",");
+                String id = lineScanner.next();
+                int block = Integer.parseInt(id.substring(0,2));
+                String type = lineScanner.next();
+                int capacity = lineScanner.nextInt();
+                boolean access = false;
+                if(lineScanner.nextInt() == 1){
+                    access = true;
+                }
+                listClassRooms.put(id, new ClassRoom(id, block, type, capacity, access));
+            }
+            scanner.close();
+        }
+        catch (Exception e){
+            System.err.println(e);
+            return null;
+        }
+        return listClassRooms;
+    }
+
+    public static HashMap<Integer, Person> getStudens() {
+        HashMap<Integer, Person> listStudents = new HashMap<>();
         try {
             Scanner scanner = new Scanner(new File("CSV/estudiantes.csv"));
-            while (scanner.hasNextLine()){
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 Scanner lineScanner = new Scanner(line);
                 lineScanner.useDelimiter(",");
                 int id = lineScanner.nextInt();
                 int mobility = lineScanner.nextInt();
-                listStudents.put(id, new Student(id, mobility));
+                listStudents.put(id, new Person(id, mobility));
             }
             scanner.close();
-            /**
-            scanner = new Scanner(new File("CSV/mat20192.csv"));
-            while (scanner.hasNextLine()){
-                String line = scanner.nextLine();
-                Scanner lineScanner = new Scanner(line);
-                lineScanner.useDelimiter(",");
-                int id = lineScanner.nextInt();
-                int course = lineScanner.nextInt();
-                listStudents.get(id).addCourses(course);
-            }
-            scanner.close();
-             */
         }
-        catch (Exception e){
+        catch (Exception e) {
             System.err.println(e);
             return null;
         }
