e35cf72a44e4ddaa022c2a10f709cc34c3d828e6
jpgomezt <jpgomeztriana@gmail.com>
2020-05-29 20:48:11 -0500

    Un debug para probarlo

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import java.sql.Time;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         HashMap<String, ClassRoom> listClassRooms = Reader.getClassrooms();
         HashMap<String, Group> listGroups = Reader.getGroups();
-        if(listClassRooms != null && listGroups != null){
+        HashMap<String, Integer> listDistances = Reader.getDistances();
+        if(listClassRooms != null && listGroups != null && listDistances != null){
             System.out.println("todo bien");
         }
         else{
