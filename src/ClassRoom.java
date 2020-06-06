c869923fe61a14d3f3075bfef1bc5cd04667760f
Santiago Alzate <salzatec1@localhost.localdomain>
2020-06-06 14:27:14 -0500

    New model

--- src/ClassRoom.java
+++ src/ClassRoom.java
@@ -136,19 +136,51 @@ public class ClassRoom{
         int distanceToCapacitySoftRestriction = Integer.MAX_VALUE;
         int capacitySoftRestriction = Integer.MAX_VALUE;
         int distanceRecorridaSoftRestriction = Integer.MAX_VALUE;
+        boolean goodAlocatedSoft = false;
+        boolean goodAlocatedHard = false;
         for(ClassRoom cr: listClassRooms.values()){
             int newDistanceRecorrida = lesson.distanceRecorrida(cr);
             if(!cr.isBooked(lesson.getCharDay(), lesson.getIntStart(), lesson.getIntEnd())){
                 if(newDistanceRecorrida < distanceRecorridaHardRestriction){
-                    if((Math.abs(cr.capacity - numPersons) < distanceToCapacityHardRestriction) && (type.equals(cr.getType())) && (!specialRoom || cr.access)){
+                    if(cr.capacity >= numPersons && type.equals(cr.getType()) && (!specialRoom || cr.access)  && !goodAlocatedHard){
+                        idCurrentHardRestriction = cr.getId();
+                        distanceToCapacityHardRestriction = Math.abs(cr.capacity - numPersons);
+                        capacityHardRestriction = cr.capacity;
+                        distanceRecorridaHardRestriction = newDistanceRecorrida;
+                        goodAlocatedHard = true;
+                    }
+                    if(cr.capacity >= numPersons && type.equals(cr.getType()) && (!specialRoom || cr.access)  && goodAlocatedHard){
+                        if(cr.capacity < capacityHardRestriction ){
+                            idCurrentHardRestriction = cr.getId();
+                            distanceToCapacityHardRestriction = Math.abs(cr.capacity - numPersons);
+                            capacityHardRestriction = cr.capacity;
+                            distanceRecorridaHardRestriction = newDistanceRecorrida;
+                        }
+                    }
+                    if((Math.abs(cr.capacity - numPersons) < distanceToCapacityHardRestriction) && (type.equals(cr.getType())) && (!specialRoom || cr.access) && !goodAlocatedHard){
                         idCurrentHardRestriction = cr.getId();
                         distanceToCapacityHardRestriction = Math.abs(cr.capacity - numPersons);
                         capacityHardRestriction = cr.capacity;
                         distanceRecorridaHardRestriction = newDistanceRecorrida;
                     }
                 }
-                if(newDistanceRecorrida < distanceRecorridaSoftRestriction) {
-                    if((Math.abs(cr.capacity - numPersons) < distanceToCapacitySoftRestriction) && (type.equals(cr.getType()))){
+                if(newDistanceRecorrida < distanceRecorridaSoftRestriction){
+                    if(cr.capacity >= numPersons && type.equals(cr.getType()) && (!specialRoom || cr.access)  && !goodAlocatedSoft){
+                        idCurrentSoftRestriction = cr.getId();
+                        distanceToCapacitySoftRestriction = Math.abs(cr.capacity - numPersons);
+                        capacitySoftRestriction = cr.capacity;
+                        distanceRecorridaSoftRestriction = newDistanceRecorrida;
+                        goodAlocatedSoft = true;
+                    }
+                    if(cr.capacity >= numPersons && type.equals(cr.getType()) && (!specialRoom || cr.access)  && goodAlocatedSoft){
+                        if(cr.capacity < capacitySoftRestriction ){
+                            idCurrentSoftRestriction = cr.getId();
+                            distanceToCapacitySoftRestriction = Math.abs(cr.capacity - numPersons);
+                            capacitySoftRestriction = cr.capacity;
+                            distanceRecorridaSoftRestriction = newDistanceRecorrida;
+                        }
+                    }
+                    if((Math.abs(cr.capacity - numPersons) < distanceToCapacitySoftRestriction) && (type.equals(cr.getType())) && !goodAlocatedSoft){
                         idCurrentSoftRestriction = cr.getId();
                         distanceToCapacitySoftRestriction = Math.abs(cr.capacity - numPersons);
                         capacitySoftRestriction = cr.capacity;
